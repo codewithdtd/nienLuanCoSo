6d1535e4cebf9b859af45bb5b213e873a7d50d3e
Dat Do <dtdat201202@gmail.com>
2023-11-29 13:11:12 +0700

    sủa cái xóa

--- delete.php
+++ delete.php
@@ -14,14 +14,20 @@ if (
     $_SERVER['REQUEST_METHOD'] === 'POST'
     && isset($_POST['id_img']) && isset($values)
 ) {
+    try {
     $values->delete($values->thumbnail);
     header('Location:admin.php?nav=suasanpham&id='.$values->product_id);
     exit();
+    }
+    catch(Exception $e) {
+        echo "<script>alert('Không thể xóa');</script>";
+    }
 
 }
 
 
 if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_product'])) {
+    try {
     $product->find($_POST['id_product']);
     $imgs = $gallery->findGallery($_POST['id_product']);
     $gallery->deleteProduct($imgs);
@@ -29,32 +35,54 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_product'])) {
     $_SESSION["success"] = "Đã xóa sản phẩm";
     header('Location:admin.php?nav=sanpham');
     exit();
+    }
+    catch(Exception $e) {
+        echo "<script>alert('Không thể xóa');</script>";
+    }
+
 
 } 
 
 if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_user'])) {
+    try {
     $user->find($_POST['id_user']); 
     $user->delete();
     header('Location:admin.php?nav=khachhang');
     exit();
+    }
+    catch(Exception $e) {
+        echo "<script>alert('Không thể xóa');</script>";
+    }
+
 
 }
 
 // xóa đơn hàng
 if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id_order'])) {
+    try {
     $statement = $conn->prepare('DELETE FROM ocake.order_details WHERE order_id = ?');
     $statement->execute([$_GET['id_order']]);
     $statement = $conn->prepare('DELETE FROM ocake.orders WHERE id = ?');
     $statement->execute([$_GET['id_order']]);
     header('Location:admin.php?nav=donhang');
     exit();
+    }
+    catch(Exception $e) {
+        echo "<script>alert('Không thể xóa');</script>";
+    }
 
 }
 
 // xóa danh mục 
 if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_category'])) {
+    try {
     $statement = $conn->prepare('DELETE FROM ocake.category WHERE id = ?');
     $statement->execute([$_POST['id_category']]);
     header('Location:admin.php?nav=category');
     exit();
+    }
+    catch(Exception $e) {
+        echo "<script>alert('Không thể xóa');</script>";
+    }
+
 }
